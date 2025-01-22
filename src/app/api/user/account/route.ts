79c06002eca9adfa6703c7c490a052b119f44e94
kisia0916 <fumiharuabe@gmail.com>
2025-01-22 16:04:48 +0900

    add create user api

--- src/app/api/user/account/route.ts
+++ src/app/api/user/account/route.ts
@@ -43,3 +43,36 @@ export async function GET(req:NextRequest):Promise<NextResponse<api_user_account
         return NextResponse.json({data:"server error"},{status:500})
     }
 }
+
+//signin
+const new_user_schema = z.object({
+    display_name:string(),
+    user_id:string()
+})
+type new_user_type = z.infer<typeof new_user_schema>
+export async function POST(req:NextRequest):Promise<NextResponse<{data:string}|api_error_response>>{
+    try{
+        const session = await auth()
+        if (session){
+            const new_user_session:user_type = user_schema.parse(session.user)
+            const body = await req.json()
+            const new_user_data:new_user_type = new_user_schema.parse(body)
+            await prisma.user.create({
+                data:{
+                    userId:new_user_data.user_id,
+                    userName:new_user_data.display_name,
+                    email:new_user_session.email,
+                    userIcon:new_user_session.image,
+                }
+            })
+            return NextResponse.json({data:"user has been created"})
+        }else{
+            return NextResponse.json({data:"session is not found"},{status:401})
+        }
+    }catch(error){
+        if (error instanceof z.ZodError){
+            return NextResponse.json({data:"validation error"},{status:500})
+        }
+        return NextResponse.json({data:"server error"},{status:500})
+    }
+}
