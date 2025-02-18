a6d3e277c5701b49f897847b3011616613731bc4
kisia0916 <fumiharuabe@gmail.com>
2025-02-18 23:02:15 +0900

    add gen checkpoint function

--- src/actions/gen_timer_checkpoint.ts
+++ src/actions/gen_timer_checkpoint.ts
@@ -1,4 +1,5 @@
 import { auth } from "$/auth"
+import jwt from "jsonwebtoken";
 
 interface token_interface {
     counter:number,
@@ -6,19 +7,34 @@ interface token_interface {
     user_id:string,
     timer_id:string
 }
-export const get_timer_checkpoint = async(token:token_interface|undefined)=>{
-    if (token){
-        
+export interface gen_timer_checkpoint_return_interface {
+    token:string,
+    state:"done"|"error"
+}
+export const get_timer_checkpoint = async(token:string|undefined):Promise<gen_timer_checkpoint_return_interface>=>{
+    const session = await auth()
+    if (session){
+        if (token){
+            const decoded_token:token_interface = jwt.verify(token,process.env.SECRET_KEY as string) as token_interface
+            const next_checkpoint = {
+                    counter:decoded_token.counter+1,
+                    timestamp:Date.now(),
+                    user_id:session.app_data.user_id,
+                    timer_id:""
+            }
+            const new_token = jwt.sign(next_checkpoint,process.env.SECRET_KEY as string)
+            return {token:new_token,state:"done"}
+        }else{
+            const first_token:token_interface = {
+                counter:0,
+                timestamp:Date.now(),
+                user_id:session.app_data.user_id,
+                timer_id:""
+            }
+            const new_token = jwt.sign(first_token,process.env.SECRET_KEY as string)
+            return {token:new_token,state:"done"}
+        }
     }else{
-        // const session = await auth()
-        // if (session){
-        //     const first_token:token_interface = {
-        //         counter:0,
-        //         timestamp:Date.now(),
-        //         user_id:session.user?.i
-        //     }
-        // }else{
-
-        // }
+        return {token:"",state:"error"}
     }
 }
